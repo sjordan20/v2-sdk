10922cd239887f2451009743b3ca937920c943c1
sjordan20 <59850993+sjordan20@users.noreply.github.com>
2022-05-25 12:15:56 -0600

    change to prod BSC factory and hash

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xca143ce32fe78f1f7019d7d551a6402fc5350c73'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
